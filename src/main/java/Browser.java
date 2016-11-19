dad559a021065b10c1ef8487ef54a1060e891a3c
MUSTAFA <furkan@gtu.edu.tr>
2016-11-19 17:05:54 +0300

    Client side without history

---
+++ src/main/java/Browser.java
@@ -0,0 +1,38 @@
+
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
+import javafx.scene.layout.Region;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Created by MUSTAFA on 19.11.2016.
+ */
+
+class Browser extends Region {
+
+    final WebView browser = new WebView();
+    final WebEngine webEngine = browser.getEngine();
+
+    public Browser() {
+        // load the home page
+        webEngine.load("http://www.google.com");
+
+        //add components
+        getChildren().add(browser);
+    }
+
+    @Override
+    protected void layoutChildren() {
+        double w = getWidth();
+        double h = getHeight();
+        layoutInArea(browser,0,0,w,h,0,HPos.CENTER,VPos.CENTER);
+    }
+
+}
